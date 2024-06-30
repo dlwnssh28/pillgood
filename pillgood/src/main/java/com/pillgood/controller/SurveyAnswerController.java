3186d42009efd6a579582cefaf66d2dbf2bdf5b8
dlwnssh28 <dlwnssh28@gmail.com>
2024-06-30 16:02:48 +0900

    controller api명 수정

--- pillgood/src/main/java/com/pillgood/controller/SurveyAnswerController.java
+++ pillgood/src/main/java/com/pillgood/controller/SurveyAnswerController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/answers")
+@RequestMapping("/surveyanswers")
 public class SurveyAnswerController {
 
     @Autowired
